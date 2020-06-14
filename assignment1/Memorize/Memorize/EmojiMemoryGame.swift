06f770af2e4581721a30d04d75740fc86f9f0b59
Le Zeng <zenglekidd@gmail.com>
2020-06-14 21:03:26 +0800

    Setup model and viewModel

---
+++ assignment1/Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Le Zeng on 2020/6/14.
+//  Copyright © 2020 Le Zeng. All rights reserved.
+//
+
+import Foundation
+
+struct EmojiMemoryGame {
+    private var game: MemoryGame<String> {
+        
+        let emojis = ["🛺","✈️","⛵️","🚢"]
+        
+        let game = MemoryGame(numberOfPairs: emojis.count) { index in
+            emojis[index]
+        }
+        
+        return game
+    }
+    
+    var cards: [MemoryGame<String>.Card] {
+        game.cards
+    }
+    
+    func choose(card: MemoryGame<String>.Card) {
+        game.chose(card)
+    }
+}
---
+++ assignment1/Memorize/Memorize/MemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Le Zeng on 2020/6/14.
+//  Copyright © 2020 Le Zeng. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    private(set) var cards: [Card]
+    
+    func chose(_ card: Card) {
+        print("card choosed: \(card)")
+    }
+    
+    init(numberOfPairs: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = []
+        for i in 0 ..< numberOfPairs {
+            let content = cardContentFactory(i)
+            cards.append(Card(content: content, id: i * 2))
+            cards.append(Card(content: content, id: i * 2 + 1))
+        }
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp = true
+        var isMatched = false
+        var content: CardContent
+        var id: Int
+    }
+}
